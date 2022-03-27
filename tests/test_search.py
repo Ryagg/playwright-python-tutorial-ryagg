3a26471e46ba07a3b5985bb1a9a2c9ed5d1fe451
Ryagg <58631455+Ryagg@users.noreply.github.com>
2022-03-27 21:44:21 +0200

    test: Add assertions about the result page

--- tests/test_search.py
+++ tests/test_search.py
@@ -1,4 +1,4 @@
-from playwright.sync_api import Page
+from playwright.sync_api import expect, Page
 
 # give test access to a fresh page in a new browser context
 def test_basic_duckduckgo_search(page: Page) -> None:
@@ -8,9 +8,16 @@ def test_basic_duckduckgo_search(page: Page) -> None:
     page.locator('#search_form_input_homepage').fill('prime lorca')
     page.locator('#search_button_homepage').click()
     # Then the search result query is the phrase
+    expect(page.locator('#search_form_input')).to_have_value('prime lorca')
     # And the search result links pertain to the phrase
+    page.locator('.result__title a.result__a').nth(4).wait_for()
+    #scrape the text contents
+    titles = page.locator('.result__title a.result__a').all_text_contents()
+    # filter the list of titles to find the ones that contain the search phrase
+    matches = [t for t in titles if 'prime lorca' in t.lower()]
+    assert len(matches) > 0
     # And the search result title contains the phrase
-    pass
+    expect(page).to_have_title('prime lorca at DuckDuckGo')
 
 # run headed test with delay of 1 sec with 'python -m pytest tests --headed
 # --slowmo 1000
@@ -18,3 +25,19 @@ def test_basic_duckduckgo_search(page: Page) -> None:
 # supported selectors:
 # Text, CSS, XPath, N-th element, React, Vue, ID attributes
 # Try to stick to text, IDs, or CSS selectors
+
+# expect connects page locator calls to Playwright's assertions
+# the expect function performs implicit waiting for the element to satisfy the
+# to_have_value condition
+# assert 'prime lorca' == page.input_value('#search_form_input') would wait for
+# the element to appear, but not for the input value to become 'prime lorca'
+
+# the selector .result__title a.result__a can be used to identify all result
+# links on a page
+
+# explicit waiting is necessary to safely mitigate potential race conditions
+
+# locator is a method that returns a Locator object for the target element. A Locator object can make many of the same calls as a page, like clicking and getting text. However, it can also make calls for explicit waiting and calls that target multiple elements.
+# .result__title a.result__a is the selector for the result links.
+# nth(4) is an N-th element fetcher. N-th element fetchers are zero-indexed and may be appended to any selector. In this call, it will fetch the fifth result link element.
+# wait_for is a method that will wait for the target element to be visible.
