aac2be8c59b0eee2ab6a0bad1f69e42f76bd6ca7
Ryagg <58631455+Ryagg@users.noreply.github.com>
2022-04-04 21:23:51 +0200

    feat: Add pytest fixtures

--- tests/test_search.py
+++ tests/test_search.py
@@ -2,10 +2,11 @@ from pages.result import DuckDuckGoResultPage
 from pages.search import DuckDuckGoSearchPage
 from playwright.sync_api import expect, Page
 
-# give test access to a fresh page in a new browser context
-def test_basic_duckduckgo_search(page: Page) -> None:
-    search_page = DuckDuckGoSearchPage(page)
-    result_page = DuckDuckGoResultPage(page)
+# fixtures are declared as arguments
+def test_basic_duckduckgo_search(
+    page: Page,
+    search_page: DuckDuckGoSearchPage,
+    result_page: DuckDuckGoResultPage) -> None:
 
     # Given the DuckDuckGo home page is displayed
     search_page.load()
@@ -25,22 +26,9 @@ def test_basic_duckduckgo_search(page: Page) -> None:
 # run headed test with delay of 1 sec with 'python -m pytest tests --headed
 # --slowmo 1000
 
-# supported selectors:
-# Text, CSS, XPath, N-th element, React, Vue, ID attributes
-# Try to stick to text, IDs, or CSS selectors
-
-# expect connects page locator calls to Playwright's assertions
-# the expect function performs implicit waiting for the element to satisfy the
-# to_have_value condition
-# assert 'prime lorca' == page.input_value('#search_form_input') would wait for
-# the element to appear, but not for the input value to become 'prime lorca'
-
-# the selector .result__title a.result__a can be used to identify all result
-# links on a page
-
-# explicit waiting is necessary to safely mitigate potential race conditions
-
-# locator is a method that returns a Locator object for the target element. A Locator object can make many of the same calls as a page, like clicking and getting text. However, it can also make calls for explicit waiting and calls that target multiple elements.
-# .result__title a.result__a is the selector for the result links.
-# nth(4) is an N-th element fetcher. N-th element fetchers are zero-indexed and may be appended to any selector. In this call, it will fetch the fifth result link element.
-# wait_for is a method that will wait for the target element to be visible.
+# if using page objects, then all interactions should be performed using page
+# objects
+# It is not recommended to mix raw Playwright calls (except expect assertions)
+# with page object calls. That becomes confusing, and it encourages poor
+# practices like dirty hacks and copypasta. It also causes a test automation
+# project to lose strength from a lack of conformity in design.
